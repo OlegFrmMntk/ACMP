76a4a7ba9319f2d4e92e625a43fbfe77ce54028a
OlegFrmMntk <Mankevich000@mail.ru>
2020-01-22 19:59:27 +0300

    Task 712

--- ACMP/ACMP.cpp
+++ ACMP/ACMP.cpp
@@ -1,56 +1,23 @@
 ﻿#include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int a[100];
-
-pair <int, bool> check(const int& size, const int& k)
-{
-	int sum = 0, mx = 0, cnt = 1;
-	for (const int& numberOfLists : a) {
-		if (numberOfLists + sum > size) {
-			mx = max(mx, sum);
-			sum = numberOfLists;
-			cnt++;
-		}
-		else sum += numberOfLists;
-	}
-	mx = max(mx, sum);
-
-	if (cnt > k) return make_pair(mx, false);
-	else return make_pair(mx, true);
-}
-
 int main()
 {
-	int n;
-	cin >> n;
+    unsigned long long w, h, n;
+    cin >> w >> h >> n;
 
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
+    unsigned long long l = 0, r = 10000000000000000000;
+    while (r - l > 1) {
+        unsigned long long mid = (l + r) / 2;
+        if (((mid / w) * (mid / h)) >= n) r = mid;
+        else l = mid;
+    }
 
-	int k;
-	cin >> k;
+    unsigned long long ans;
+    if (((l / w) * (l / h)) >= n) ans = l;
+    else ans = r;
 
-	int l = 0;
-	for (auto numberOfLists : a)
-		l = max(l, numberOfLists);
-
-	int ans = INT_MAX, r = 32767, mx;
-	while (r - l > 1) {
-		int mid = (l + r) / 2;
-
-		if (check(mid, k).second) r = mid;
-		else l = mid;
-	}
-
-	mx = check(l, k).first;
-	if (check(l, k).second) {
-		ans = min(ans, mx);
-	}
-	else ans = min(ans, check(r, k).first);
-
-	cout << ans;
-	return 0;
+    cout << ans;
+    return 0;
 }
